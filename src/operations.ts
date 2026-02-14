202532a48307640f8fddf2b8769db6152b5d192e
Clinton Selke <clinuxrulz@gmail.com>
2026-02-14 20:26:03 +1000

    code to write the operation tree to the brickmap for a given region

--- src/operations.ts
+++ src/operations.ts
@@ -1,9 +1,11 @@
 import * as THREE from "three";
+import { BrickMap } from "./BrickMap";
 
 export type Operation = {
   index: number,
   combinedType: "Add" | "Subtract",
-  transform: THREE.Matrix4,
+  origin: THREE.Vector3,
+  orientation: THREE.Quaternion,
   operationShape: OperationShape,
 };
 
@@ -18,6 +20,57 @@ export type OperationShape = {
   lenX: number,
   radius: number,
 };
+
+let _operationEvalSDF_pt = new THREE.Vector3();
+let _operationEvalSDF_q = new THREE.Quaternion();
+export function operationEvalSDF(operation: Operation, pt: THREE.Vector3): number {
+  let q = _operationEvalSDF_q.copy(operation.orientation).conjugate();
+  let pt2 = _operationEvalSDF_pt.copy(pt).sub(operation.origin).applyQuaternion(q);
+  let shape = operation.operationShape;
+  switch (shape.type) {
+    case "Ellipsoid":
+      return ellipsoidSDF(shape.radius, pt);
+    case "Box":
+      return boxSDF(shape.len, pt);
+    case "Capsule":
+      return capsuleSDF(shape.lenX, shape.radius, pt);
+  }
+}
+
+let _ellipsoidSDF_pt1 = new THREE.Vector3();
+let _ellipsoidSDF_pt2 = new THREE.Vector3();
+let _ellipsoidSDF_pt3 = new THREE.Vector3();
+function ellipsoidSDF(radius: THREE.Vector3, pt: THREE.Vector3): number {
+  let pt2 = _ellipsoidSDF_pt1.copy(pt).divide(radius);
+  let rr = _ellipsoidSDF_pt2.copy(radius).multiply(radius);
+  let pt3 = _ellipsoidSDF_pt3.copy(pt).divide(rr);
+  let k1 = pt2.length();
+  let k2 = pt3.length();
+  return k1 * (k1 - 1.0) / k2;
+}
+
+let _boxSDF_b = new THREE.Vector3();
+let _boxSDF_q = new THREE.Vector3();
+function boxSDF(len: THREE.Vector3, pt: THREE.Vector3): number {
+  let b = _boxSDF_b.copy(len).multiplyScalar(0.5);
+  let q = _boxSDF_q.copy(pt);
+  q.x = Math.abs(q.x);
+  q.y = Math.abs(q.y);
+  q.z = Math.abs(q.z);
+  q.sub(b);
+  let c = Math.min(Math.max(q.x, q.y, q.z), 0.0);
+  q.x = Math.max(q.x, 0.0);
+  q.y = Math.max(q.y, 0.0);
+  q.z = Math.max(q.z, 0.0);
+  return q.length() + c;
+}
+
+let _capsuleSDF_pt2 = new THREE.Vector3();
+function capsuleSDF(lenX: number, radius: number, pt: THREE.Vector3): number {
+  let pt2 = _capsuleSDF_pt2.copy(pt);
+  pt2.x = Math.max(0.0, Math.min(lenX, pt2.x));
+  return pt.distanceTo(pt2) - radius;
+}
 
 let _isOperationPossiblyInAabb_vertices: [
   THREE.Vector3, THREE.Vector3, THREE.Vector3, THREE.Vector3,
@@ -94,14 +147,14 @@ export function operationCornerPoints(
   let shape = operation.operationShape;
   let aabb = _operationCornerPoints_aabb;
   operationShapeBoundingBox(shape, aabb);
-  out[0].set(aabb.min.x, aabb.min.y, aabb.min.z).applyMatrix4(operation.transform);
-  out[1].set(aabb.min.x, aabb.min.y, aabb.max.z).applyMatrix4(operation.transform);
-  out[2].set(aabb.min.x, aabb.max.y, aabb.min.z).applyMatrix4(operation.transform);
-  out[3].set(aabb.min.x, aabb.max.y, aabb.max.z).applyMatrix4(operation.transform);
-  out[4].set(aabb.max.x, aabb.min.y, aabb.min.z).applyMatrix4(operation.transform);
-  out[5].set(aabb.max.x, aabb.min.y, aabb.max.z).applyMatrix4(operation.transform);
-  out[6].set(aabb.max.x, aabb.max.y, aabb.min.z).applyMatrix4(operation.transform);
-  out[7].set(aabb.max.x, aabb.max.y, aabb.max.z).applyMatrix4(operation.transform);
+  out[0].set(aabb.min.x, aabb.min.y, aabb.min.z).applyQuaternion(operation.orientation).add(operation.origin);
+  out[1].set(aabb.min.x, aabb.min.y, aabb.max.z).applyQuaternion(operation.orientation).add(operation.origin);
+  out[2].set(aabb.min.x, aabb.max.y, aabb.min.z).applyQuaternion(operation.orientation).add(operation.origin);
+  out[3].set(aabb.min.x, aabb.max.y, aabb.max.z).applyQuaternion(operation.orientation).add(operation.origin);
+  out[4].set(aabb.max.x, aabb.min.y, aabb.min.z).applyQuaternion(operation.orientation).add(operation.origin);
+  out[5].set(aabb.max.x, aabb.min.y, aabb.max.z).applyQuaternion(operation.orientation).add(operation.origin);
+  out[6].set(aabb.max.x, aabb.max.y, aabb.min.z).applyQuaternion(operation.orientation).add(operation.origin);
+  out[7].set(aabb.max.x, aabb.max.y, aabb.max.z).applyQuaternion(operation.orientation).add(operation.origin);
 }
 
 export function operationShapeBoundingBox(shape: OperationShape, out: THREE.Box3) {
@@ -137,6 +190,7 @@ let _getOperationWorldBounds_vertices: [
   new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3(),
   new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3(),
 ];
+
 export function getOperationWorldBounds(
   op: Operation,
   out: THREE.Box3,
@@ -271,5 +325,54 @@ export class OperationBVH {
     combined.getSize(size);
     // Surface Area
     return 2 * (size.x * size.y + size.y * size.z + size.z * size.x);
+  }
+
+  private _updateBrickMap_aabb = new THREE.Box3();
+  private _updateBrickMap_pt = new THREE.Vector3();
+  updateBrickMap(
+    brickMap: BrickMap,
+    minXIdx: number,
+    minYIdx: number,
+    minZIdx: number,
+    maxXIdx: number,
+    maxYIdx: number,
+    maxZIdx: number
+  ) {
+    let operations: Operation[] = [];
+    let aabb = this._updateBrickMap_aabb;
+    let minX = (minXIdx - 512) * 10.0;
+    let minY = (minYIdx - 512) * 10.0;
+    let minZ = (minZIdx - 512) * 10.0;
+    let pt = this._updateBrickMap_pt;
+    for (let i = minZIdx, atMinZ = minZ; i <= maxZIdx; ++i, atMinZ += 10.0) {
+      for (let j = minYIdx, atMinY = minY; j <= maxYIdx; ++j, atMinY += 10.0) {
+        for (let k = minXIdx, atMinX = minX; k <= maxXIdx; ++k, atMinX += 10.0) {
+          aabb.min.set(atMinX - 10.0, atMinY - 10.0, atMinZ - 10.0);
+          aabb.max.set(atMinX + 10.0, atMinY + 10.0, atMinZ + 10.0);
+          operations.splice(0, operations.length);
+          this.query(aabb, operations);
+          operations.sort((a, b) => a.index - b.index);
+          pt.copy(aabb.min);
+          let dist = Number.POSITIVE_INFINITY;
+          for (let operation of operations) {
+            let a = operationEvalSDF(operation, pt);
+            switch (operation.combinedType) {
+              case "Add":
+                dist = Math.min(dist, a);
+                break;
+              case "Subtract":
+                dist = Math.max(dist, -a);
+                break;
+            }
+          }
+          if (Number.isFinite(dist)) {
+            let a = Math.max(1, Math.min(255, 128 - Math.floor(127.0 * dist / 10.0)));
+            brickMap.set(k, j, i, a);
+          } else {
+            brickMap.set(k, j, i, 0);
+          }
+        }
+      }
+    }
   }
 }
